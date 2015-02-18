f3b0642f1b13e406184d8da828d912f6685d6f56
pbadun <pbadun@gmail.com>
2015-02-18 08:34:49 +0300

    Revert "null"
    This reverts commit 6e79284d20e68f063ac203fcd14a0bdc6ba239f8.

--- src/com/example/gm3/framework/Audio.java
+++
@@ -1,8 +0,0 @@
-package com.example.gm3.framework;
-
-public interface Audio {
-
-	public Music newMusic(String fileName);
-
-	public Sound newSound(String fileName);
-}
--- src/com/example/gm3/framework/Sound.java
+++
@@ -1,8 +0,0 @@
-package com.example.gm3.framework;
-
-public interface Sound {
-	
-	public void play(float volume);
-
-	public void dispose();
-}
